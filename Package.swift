d4c9ecde4f83165b16a226ff4accd55443f1cffb
Vilijan Monev <monevvilijan@gmail.com>
2021-11-14 09:56:09 +0100

    Update Package.swift
    updated iOS versions.

--- Package.swift
+++ Package.swift
@@ -6,10 +6,7 @@ import PackageDescription
 let package = Package(
     name: "swift-algorand-sdk",
     platforms: [
-          .macOS(.v10_12),
-           .iOS(.v10),
-           .tvOS(.v10),
-           .watchOS(.v3)
+          .iOS(.v13)
       ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
